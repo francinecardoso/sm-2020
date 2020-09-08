507a6f05ff2abc6ad4e59d9ad83a42a7e9b91808
Francine Cardoso <francinejfs@hotmail.com>
2020-09-08 17:06:10 -0300

    feat: câmera

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,17 @@ export function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
+      <Button 
         title="About"
         onPress={() => navigation.navigate('About')}
       />
       <Button
         title="Profile"
         onPress={() => navigation.navigate('Profile')}
+      />
+      <Button
+        title="Camera"
+        onPress={() => navigation.navigate('Camera')}
       />
     </View>
   );
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
-import 'react-native-gesture-handler'; 
 import React from 'react';
+import 'react-native-gesture-handler'; 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -7,16 +7,30 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Home } from './../pages/Home'
 import { About } from './../pages/About'
 import { Profile } from './../pages/Profile'
+import { OpenCamera } from '../pages/OpenCamera'
 
 const Stack = createStackNavigator();
 
 export function Routes(){
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="4137">
-                <Stack.Screen  name="4137" component={Home} />
-                <Stack.Screen name="About" component={About} />
-                <Stack.Screen name="Profile" component={Profile} />
+            <Stack.Navigator initialRouteName="4137"  headerMode="none" >
+                <Stack.Screen  
+                    name="4137" 
+                    component={Home}                
+                />
+                <Stack.Screen 
+                    name="About" 
+                    component={About} 
+                />
+                <Stack.Screen 
+                    name="Profile" 
+                    component={Profile} 
+                />
+                <Stack.Screen
+                    name="Camera"
+                    component={OpenCamera}
+                />
             </Stack.Navigator>
         </NavigationContainer>
       );
