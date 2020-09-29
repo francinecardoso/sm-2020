f20a962745cee64aee31c19a6a2018822c7aa9e2
Francine Cardoso <francinejfs@hotmail.com>
2020-09-29 14:08:29 -0300

    feat: useState e props

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export function Profile() {
+export function Profile(props) {
+
+    const {imc} = props.route.params;
     return (
-        <View>
-            <Text></Text>
+        <View  style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>IMC: {imc.toFixed(2)}</Text>
         </View>
     )
 }
